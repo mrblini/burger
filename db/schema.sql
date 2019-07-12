fbad6aa9ff679e732648ed76820f01d33c1b5ad8
pablodiazt <pablodiaztr@gmail.com>
2019-07-11 23:34:29 -0500

    changes

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int AUTO_INCREMENT NOT NULL,
+	burger_name varchar(50) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('King Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Ya Fav Burger Baby!');
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Ultimum Bacon Tower Burger', true);
+    
+
